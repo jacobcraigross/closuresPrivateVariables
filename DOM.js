5b012ebebe2fb90a8e38800b34609e202d756c79
Jacob Ross <survivaldawn@gmail.com>
2021-01-23 07:50:51 -0600

    Update DOM.js

--- DOM.js
+++ DOM.js
@@ -1,11 +1,29 @@
-var body = document.querySelector("body");  
-var isRed = false;
+var counter = (function() {
+  var privateCounter = 0;
+  function changeBy(val) {
+    privateCounter += val;
+  }
 
-setInterval(function(){  
-	if (isRed) {
-		body.style.background = "white";
-	} else {
-		body.style.background = "#EE2C2C";
-	}
-	isRed = !isRed;
-}, 1000);
+  return {
+    increment: function() {
+      changeBy(1);
+    },
+
+    decrement: function() {
+      changeBy(-1);
+    },
+
+    value: function() {
+      return privateCounter;
+    }
+  };
+})();
+
+console.log(counter.value());  // 0.
+
+counter.increment();
+counter.increment();
+console.log(counter.value());  // 2.
+
+counter.decrement();
+console.log(counter.value());  // 1.
